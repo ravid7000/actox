5c48907de68412bc6c2c6ea9fff01bef18af25d8
Ravi Dhiman <ravi.dhiman@tokopedia.com>
2019-12-03 10:32:44 +0530

    removed old dependencies

--- bundle.js
+++
@@ -1,4 +0,0 @@
-import createStore from './src/actox'
-import { getState, subscribe, dispatch, addState, updateState } from './src/actox'
-
-export { createStore, getState, subscribe, dispatch, addState, updateState }
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import createStore from './actox'
-import connect from './connect'
-import { getState, subscribe, dispatch, addState, updateState } from './actox'
-
-export { createStore, getState, subscribe, dispatch, addState, updateState, connect }
